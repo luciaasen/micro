14f455ce18c2c1060de589efe2d6351201fbf6cf
luciaasen <luciaasen@gmail.com>
2018-04-02 01:19:00 +0200

    at last

--- 2p/labs2a.asm
+++ 2p/labs2a.asm
@@ -22,6 +22,16 @@ matrix db 1, 0, 0, 0; ;We allocate the 7x4 Transposed Generation Matrix
        db 1, 0, 1, 1
        db 0, 1, 1, 1
 
+input db 'Input: "X X X X"', 13, 10, '$'
+output db 'Output: "X X X X X X X"', 13, 10, '$'
+comp db 'Computation:', 13, 10, '$'
+fin db '"', 13, 10, '$'
+ini db  '      | P1 | P2 | D1 | P4 | D2 | D3 | D4', 13, 10, '$'
+word_ db 'Word  | ?  | ?  | X  | ?  | X  | X  | X', 13, 10, '$'
+p1 db   'P1    | X  |    | X  |    | X  |    | X', 13, 10, '$'
+p2 db   'P2    |    | X  | X  |    |    | X  | X', 13, 10, '$'
+p4 db   'P4    |    |    |    | X  | X  | X  | X', 13, 10, '$'
+
 DATOS ENDS
 
 ;**************************************************************************
@@ -50,6 +60,10 @@ INICIO PROC
 
     ; Calling the function that multiplies vector x matrix
     CALL MULTIPLY
+	
+	; PROGRAM END
+	MOV AX, 4C00H
+	INT 21H
 
 INICIO ENDP
 
@@ -62,6 +76,11 @@ INICIO ENDP
 ;__________________________________________________________________________ 
 
 MULTIPLY PROC NEAR
+    ; We could not do just a loop to multiply the whole vector, 
+    ; as it was contained in two different registers
+    ; Therefore, we needed 2 loops, and each of them having 2 iterations
+    ; We thought expanding the loop was a better solution as, in the end,
+    ; it was easier to understand than the 2 double loops
 
     MOV SI, 0 ; DI will be the result vector index and matrix row index 
 EACH_ROW:
@@ -72,11 +91,10 @@ EACH_ROW:
     MOV CL, 2                       ; We want to shift twice
     SHL DI, CL                      ; DI <= 4*DI
     
-    MOV AL, BYTE PTR DH             ; Factor1 = input vector BP-th element
+    MOV AL, BYTE PTR DH             ; Factor1 = input vector 1st element
     MUL matrix[0][DI]               ; Factor2 = Matrix element multiplied
-    ADD result[SI], BYTE PTR AL     ; Result in AX is added to what we had in  
-    ; SERA AX[0] o AX[1]
-
+    ADD result[SI], BYTE PTR AL     ; Result in AX is added to what we had   
+                                    ; in AL
     INC SI                          ; Increment the matrix rows index
     CMP SI, 7                       ; Until we have multiplied the 7 rows
     JNZ EACH_ROW 
@@ -91,10 +109,10 @@ EACH_ROW2:
     MOV CL, 2                       ; We want to shift twice
     SHL DI, CL                      ; DI <= 4*DI
     
-    MOV AL, BYTE PTR DL             ; Factor1 = input vector BP-th element
+    MOV AL, BYTE PTR DL             ; Factor1 = input vector 2nd element
     MUL matrix[1][DI]               ; Factor2 = Matrix element multiplied
-    ADD result[SI], BYTE PTR AL     ; Result in AX is added to what we had in  
-
+    ADD result[SI], BYTE PTR AL     ; Result in AX is added to what we had  
+                                    ; in AL
     INC SI                          ; Increment the matrix rows index
     CMP SI, 7                       ; Until we have multiplied the 7 rows
     JNZ EACH_ROW2 
@@ -109,10 +127,10 @@ EACH_ROW3:
     MOV CL, 2                       ; We want to shift twice
     SHL DI, CL                      ; DI <= 4*DI
     
-    MOV AL, BYTE PTR BH             ; Factor1 = input vector BP-th element
+    MOV AL, BYTE PTR BH             ; Factor1 = input vector 3rd element
     MUL matrix[2][DI]               ; Factor2 = Matrix element multiplied
-    ADD result[SI], BYTE PTR AL     ; Result in AX is added to what we had in  
-
+    ADD result[SI], BYTE PTR AL     ; Result in AX is added to what we had
+                                    ; in AL
     INC SI                          ; Increment the matrix rows index
     CMP SI, 7                       ; Until we have multiplied the 7 rows
     JNZ EACH_ROW3 
@@ -128,10 +146,10 @@ EACH_ROW4:
     MOV CL, 2                       ; We want to shift twice
     SHL DI, CL                      ; DI <= 4*DI
     
-    MOV AL, BYTE PTR BL             ; Factor1 = input vector BP-th element
+    MOV AL, BYTE PTR BL             ; Factor1 = input vector 4th element
     MUL matrix[3][DI]               ; Factor2 = Matrix element multiplied
-    ADD result[SI], BYTE PTR AL     ; Result in AX is added to what we had in  
-
+    ADD result[SI], BYTE PTR AL     ; Result in AX is added to what we had
+                                    ; in AL
     INC SI                          ; Increment the matrix rows index
     CMP SI, 7                       ; Until we have multiplied the 7 rows
     JNZ EACH_ROW4 
@@ -141,14 +159,14 @@ EACH_ROW4:
     ; the result of adding 1's and 0's)
     ; We use 8-bit division because the greatest number we are going to
     ; divide is 1*1 + 1*1 + 1*1 + 1*1 = 4 
-    MOV CX, 2 ; CX <= 2 because we are working modulo 2, 2 is the divisor
+    MOV CL, 2 ; CL <= 2 because we are working modulo 2, 2 is the divisor
     MOV DI, 0 ; DI will index the result vector
     
 
 MODULO:
-    MOV AH, 00h                    ; AX is the dividend = AH:AL = 00h:result[DI]
+    MOV AH, 00h                    ; AX is the dividend: AH:AL = 00h:result[DI]
     MOV AL, result[DI]             
-    DIV CX                         ; AX/2
+    DIV CL                         ; AX/2
     MOV result [DI], AH            ; result[DI] <= remainder
 
     INC DI                         ; We repeat for each element
@@ -160,6 +178,153 @@ MODULO:
     MOV AX, OFFSET result   ; AX contains the OFFSET of the result vector.
  
 
+    ; Last part: printing
+    MOV CX, DX              ; We need to preserve the value of DX
+                            ; since it contains part of the input
+                               
+    ; ---------- INPUT LINE -----------
+    ; We fill the 'X' in the input string with the input vector
+    ;To get the ASCII code of register, we add it up with "0"    
+    MOV AL, result[0]
+    ADD AL, "0"
+    MOV input[8], AL
+    MOV AL, result[1]
+    ADD AL, "0"
+    MOV input[10], AL
+    MOV AL, result[2]
+    ADD AL, "0"
+    MOV input[12], AL
+    MOV AL, result[3]
+    ADD AL, "0"
+    MOV input[14], AL
+    
+    ;We print the input string
+    MOV DX, OFFSET input    ; DX has the offset of the string
+    MOV AH, 9               ; Function 9: print ascii string
+    INT 21H
+   
+    ; ---------- OUTPUT LINE ----------
+    ;We fill the 'X' of the output string with the reordered output vector
+    ;To get the ASCII code of register, we add it up with "0"    
+    MOV AL, result[4]
+    ADD AL, "0"
+    MOV output[9], AL    ;P1
+    MOV AL, result[5]
+    ADD AL, "0"
+    MOV output[11], AL   ;P2
+    MOV AL, result[0]
+    ADD AL, "0"
+    MOV output[13], AL   ;D1
+    MOV AL, result[6]
+    ADD AL, "0"    
+    MOV output[15], AL   ;P4
+    MOV AL, result[1]
+    ADD AL, "0"    
+    MOV output[17], AL   ;D2
+    MOV AL, result[2]
+    ADD AL, "0"    
+    MOV output[19], AL   ;D3
+    MOV AL, result[3]
+    ADD AL, "0"    
+    MOV output[21], AL   ;D4
+
+    ;We print the output string
+    MOV DX, OFFSET output    ; DX has the offset of the string
+    MOV AH, 9               ; Function 9: print ascii string
+    INT 21H
+         
+    ; -------- COMPUTATION MATRIX -------
+    ;We print the computation line
+    ;To get the ASCII code of register, we add it up with "0"    
+    MOV DX, OFFSET comp     ; DX has the offset of the string
+    MOV AH, 9               ; Function 9: print ascii string
+    INT 21H 
+    
+    ;We print the first line
+    MOV DX, OFFSET ini      ; DX has the offset of the string
+    MOV AH, 9               ; Function 9: print ascii string
+    INT 21H
+    
+    ;We fill the 'X' in 'Word...' line with D1, D2, D3, D4
+    ;To get the ASCII code of register, we add it up with "0"    
+    MOV AL, result[0]
+    ADD AL, "0"    
+    MOV word_[18], AL
+    MOV AL, result[1]
+    ADD AL, "0"    
+    MOV word_[28], AL
+    MOV AL, result[2]
+    ADD AL, "0"    
+    MOV word_[33], AL
+    MOV AL, result[3]
+    ADD AL, "0"    
+    MOV word_[38], AL
+
+    ; We print the 'Word...' line
+    MOV DX, OFFSET word_     ; DX has the offset of the string
+    MOV AH, 9               ; Function 9: print ascii string
+    INT 21H 
+ 
+    ;We fill the 'X' in the 'P1...' line with P1, D1, D2, D4
+    ;To get the ASCII code of register, we add it up with "0"    
+    MOV AL, result[4]
+    ADD AL, "0"    
+    MOV p1[8], AL  
+    MOV AL, result[0]
+    ADD AL, "0"    
+    MOV p1[18], AL
+    MOV AL, result[1]
+    ADD AL, "0"    
+    MOV p1[28], AL
+    MOV AL, result[3]
+    ADD AL, "0"    
+    MOV p1[38], AL
+
+    ; We print the 'P1...' line
+    MOV DX, OFFSET p1       ; DX has the offset of the string
+    MOV AH, 9               ; Function 9: print ascii string
+    INT 21H 
+
+    ;We fill the 'X' in the 'P2...' line with P2, D1, D3, D4
+    ;To get the ASCII code of register, we add it up with "0"    
+    MOV AL, result[5]
+    ADD AL, "0"    
+    MOV p2[13], AL
+    MOV AL, result[0]
+    ADD AL, "0"    
+    MOV p2[18], AL
+    MOV AL, result[2]
+    ADD AL, "0"    
+    MOV p2[33], AL
+    MOV AL, result[3]
+    ADD AL, "0"    
+    MOV p2[38], AL
+
+    ; We print the 'P2...' line
+    MOV DX, OFFSET p2       ; DX has the offset of the string
+    MOV AH, 9               ; Function 9: print ascii string
+    INT 21H 
+    
+    ;We fill the 'X' in the 'P3...' line with P4, D2, D3, D4
+    ;To get the ASCII code of register, we add it up with "0"    
+    MOV AL, result[6]
+    ADD AL, "0"    
+    MOV p4[23], AL
+    MOV AL, result[1]
+    ADD AL, "0"    
+    MOV p4[28], AL
+    MOV AL, result[2]
+    ADD AL, "0"    
+    MOV p4[33], AL
+    MOV AL, result[3]
+    ADD AL, "0"    
+    MOV p4[38], AL
+
+    ; We print the 'P4...' line
+    MOV DX, OFFSET p4       ; DX has the offset of the string
+    MOV AH, 9               ; Function 9: print ascii string
+    INT 21H 
+ 
     RET
 MULTIPLY ENDP
 
